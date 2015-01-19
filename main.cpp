6a2f1c7941542df14a5d9cd711b4391d0e405110
mmmn <mmn@mnmn.me>
2015-01-18 21:00:56 -0800

    Rename Launcher.dat.backup back to Launcher.dat

--- main.cpp
+++ main.cpp
@@ -109,6 +109,7 @@ int main(int argc, char* argv[]) {
     }
     rm(sdDrive + nccInfo);
     rm(sdDrive + launcherdat);
+    mv(sdDrive + launcherdat + ".autobackup", sdDrive + launcherdat);
     rm(rom);
     rm(forSD + nccInfo);
 
